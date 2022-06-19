d5c31a010fecfda7e837e6cc9919daafa416df43
Rngharmanpro <102199923+Rngharmanpro@users.noreply.github.com>
2022-06-19 16:22:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def start(bot, message):
         "I'm Link Shortener bot. Just send me link and get short link, You can also send multiple links seperated by a space or enter.\n\n**Developer:** @AKBotZ")
 
 
-@akbotz.on_message(filters.private & filters.text & filters.incoming)
+@CyniteBots.on_message(filters.private & filters.text & filters.incoming)
 async def link_handler(bot, message):
     link_pattern = re.compile('https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}', re.DOTALL)
     links = re.findall(link_pattern, message.text)
@@ -54,4 +54,4 @@ async def get_shortlink(link):
             return data["shortenedUrl"]
 
 
-akbotz.run()
+CyniteBots.run()
